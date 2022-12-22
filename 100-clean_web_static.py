bf9c1ecaa9f2ecb699ad1d4ef313e9daf748016f
anitahx <anitaizzy4@gmail.com>
2022-12-22 10:39:42 -0800

    web static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-# Fabfile to delete out-of-date archives.
-import os
+"""delete out-of-date archives"""
 from fabric.api import *
+import os
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ['3.238.181.140', '3.236.65.72']
 
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
-    Args:
-        number (int): The number of archives to keep.
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
+    """clean database of old archives
+        Where number is the no. of archives to keep
     """
+
     number = 1 if int(number) == 0 else int(number)
 
+    # this arranges the archives from oldest - newest
     archives = sorted(os.listdir("versions"))
+    # remove and return the newest
     [archives.pop() for i in range(number)]
+
+    # rm the remaining old archive files after pop
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        [local('sudo rm ./{}'.format(a)) for a in archives]
 
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
+        # make sure that files are listed from oldest to newest on server
+        archives = run('ls -tr').split()
+        # the new archive list will now contain only folders starting with web_
+        archives = [a for a in archives if 'web_static_' in a]
         [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        # seperate the files to keep and remove unwanted
+        [run('sudo rm -rf ./{}'.format(a)) for a in archives]
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,31 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+"""create fabfile"""
+from fabric.api import put, run, env
+from fabric.contrib import files
+import os
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ['3.238.181.140', '3.236.65.72']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if os.path.isfile(archive_path) is False:
+    """deploy function"""
+    if not os.path.exists(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    data_path = '/data/web_static/releases/'
+    temp = archive_path.split('.')[0]
+    file_name = temp.split('/')[1]
+    dest = data_path + file_name
+
+    try:
+        put(archive_path, '/tmp')
+        run('sudo mkdir -p {}'.format(dest))
+        run('sudo tar -xzf /tmp/{}.tgz -C {}'.format(file_name, dest))
+        run('sudo rm -f /tmp/{}.tgz'.format(file_name))
+        run('sudo mv {}/web_static/* {}/'.format(dest, dest))
+        run('sudo rm -rf {}/web_static/'.format(dest))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {} /data/web_static/current'.format(dest))
+        return True
+    except RuntimeError:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
